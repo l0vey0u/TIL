c4157b8f5ea570fac307e158f2efb0c0d82871b5
l0vey0u <duckhoim@kyonggi.ac.kr>
2019-11-02 04:07:51 +0000

    TEST modify path / to full url  20191102

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,7 @@
 module.exports = {
 	title: 'TIL',
 	description: 'Today I Learned',
-	base: "/",
+	base: "http://til.syru.dev",
 	themeConfig: {
 		nav: [
 			{ text: 'GitHub', link: 'https://github.com/l0vey0u' },
